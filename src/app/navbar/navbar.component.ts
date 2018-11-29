4a0ae04fd6ae9cfc99f80dd9d09c54c05bf15b1b
MichaelYungLee <mikenikey@g.ucla.edu>
2018-11-29 11:59:57 -0800

    Added login and navbar components, generated mobile screens for /home and /login

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private location: Location) { }
+
+  ngOnInit() {
+  }
+
+}
